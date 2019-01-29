7edce778e4469ec13d05401ef8e1b8a920c0f5e6
Patryk Robakowski <patryk.robakowski@outlook.com>
2019-01-29 11:10:36 +0100

    // WIP menu

--- app/src/main/java/com/darthside/marvelissimo/MainActivity.kt
+++ app/src/main/java/com/darthside/marvelissimo/MainActivity.kt
@@ -58,22 +58,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_home -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_series -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_characters -> {
 
             }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
+            R.id.nav_favourites -> {
 
             }
         }
