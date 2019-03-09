b418c6cfe9eecc1f57ae0b4cbea2feb37e11e483
Fatlum Haliti <fatlum93@gmail.com>
2019-03-09 18:52:38 +0100

    Frontend for characters is done

--- app/src/main/java/com/darthside/marvelissimo/api/APICaller.kt
+++ app/src/main/java/com/darthside/marvelissimo/api/APICaller.kt
@@ -166,4 +166,31 @@ class APICaller {
             }
         })
     }
+    fun getCharacterById(callback: (List<CharacterDTO>) -> Unit, characterId: Int){
+
+        url = "https://gateway.marvel.com/v1/public/characters/$characterId?&ts=$ts&apikey=$apiKey&hash=$hash"
+        Log.d(httpTag, "Attempting request")
+        val request = Request.Builder().url(url).build()
+
+        client.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                val response = response.body()?.string()
+                println("CharacterDataWrapper body: $response")
+
+                val gson = GsonBuilder().create()
+                val jsonData = gson.fromJson(response, CharacterDataWrapper::class.java)
+
+                if (jsonData.data.results.isNotEmpty()) {
+                    val characterList = jsonData.data.results
+                    callback(characterList)
+                } else {
+                    Log.d(httpTag, "No character details found when calling getCharacetDetails")
+                }
+            }
+
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                println("Failed to execute request")
+            }
+        })
+    }
 }
--- app/src/main/java/com/darthside/marvelissimo/main_files/CharacterDetailsActivity.kt
+++ app/src/main/java/com/darthside/marvelissimo/main_files/CharacterDetailsActivity.kt
@@ -1,23 +1,52 @@
 package com.darthside.marvelissimo.main_files
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.darthside.marvelissimo.R
-
-import kotlinx.android.synthetic.main.activity_character_details.*
+import com.darthside.marvelissimo.api.APICaller
 
 class CharacterDetailsActivity : AppCompatActivity() {
+
+    private val apiCaller = APICaller()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character_details)
-        var text : TextView = findViewById(R.id.characterDetails_TextView)
 
         val passedId = intent.getIntExtra("id", 0)
 
-        text.text = "$passedId"
+        var characterimagePh : ImageView? = findViewById(R.id.image_ph)
+        var namePh : TextView? = findViewById(R.id.character_title_ph)
+        var descriptionPh : TextView? = findViewById(R.id.character_description)
+        var seriesListLayout : LinearLayout = findViewById(R.id.series_list_layout)
+
+
+        apiCaller.getCharacterById ({
+
+            runOnUiThread {
+                for (c in it) {
+                    //imagePh = s.thumbnail
+                    namePh?.text = c.name
+                    descriptionPh?.text = c.description
+
+                    for (c in c.series.items) {
+                        var seriesName = c.name
+                        var textView = TextView(this)
+                        textView.setTextColor(ContextCompat.getColor(this, R.color.colorText))
+                        textView.text = seriesName
+                        seriesListLayout.addView(textView)
+                    }
+
+                }
+            }
+        }, passedId)
+
+
+
     }
 
 }
