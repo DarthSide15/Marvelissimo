c490ec3f71b6c6d6b94bee1d64c0757bf67bb2b8
Patryk Robakowski <patryk.robakowski@outlook.com>
2019-03-11 22:11:20 +0100

    added wiki url to character details view

--- app/src/main/java/com/darthside/marvelissimo/main_files/CharacterDetailsActivity.kt
+++ app/src/main/java/com/darthside/marvelissimo/main_files/CharacterDetailsActivity.kt
@@ -1,8 +1,11 @@
 package com.darthside.marvelissimo.main_files
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -23,6 +26,7 @@ class CharacterDetailsActivity : AppCompatActivity() {
         var namePh : TextView? = findViewById(R.id.character_title_ph)
         var descriptionPh : TextView? = findViewById(R.id.character_description)
         var seriesListLayout : LinearLayout = findViewById(R.id.series_list_layout)
+        var urlButton : Button = findViewById(R.id.url_button)
 
 
         apiCaller.getCharacterById ({
@@ -32,6 +36,13 @@ class CharacterDetailsActivity : AppCompatActivity() {
                     //imagePh = s.thumbnail
                     namePh?.text = c.name
                     descriptionPh?.text = c.description
+                    var wikiUrl = c.urls[1].url
+                    urlButton.text = "More details"
+                    urlButton.setOnClickListener {
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.setData(Uri.parse(wikiUrl))
+                        startActivity(intent)
+                    }
 
                     for (c in c.series.items) {
                         var seriesName = c.name
